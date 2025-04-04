cbaf11dc92abdaa7fccf28a0a7c2f018253f3334
yubinx <yubinxie222@gmail.com>
2025-04-04 13:39:55 -0400

    Add required variables for PenguinArea

--- Assets/penguin/Scripts/PenguinArea.cs
+++ Assets/penguin/Scripts/PenguinArea.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
-
-public class NewEmptyCSharpScript
+using Unity.MLAgents;
+using TMPro;
+using System.Collections.Generic;
+public class PenguinArea: MonoBehaviour
 {
-    
+    [Tooltip("The agent inside the area")]
+    public PenguinAgent penguinAgent;
+
+    [Tooltip("The baby penguin inside the area")]
+    public GameObject babyPenguin;
+
+    [Tooltip("The TextmeshPro that shows the cumulative reward of the agent")]
+    public TextMesh cumulativeRewardText;
+
+    [Tooltip("Prefab of a live fish")]
+    public Fish fishPrefab;
+
+    // A list of all the fish in the area
+    private List<Fish> fishList;
 }
